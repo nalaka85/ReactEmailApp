1e0a6875c4476359cf25f3f7b4ef6cd3fe594ddf
Nalaka <ndvrocks@gmail.com>
2020-05-25 15:00:11 +0100

    finished authflw

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 
 //Import express library node JS currently has access to common js  in react you have eg import express from express
 const express = require('express');
-const app = express(); //inside one app you can have mny
-// Route Handler
-app.get('/', (req, res) => {  // req incoming request res is the response
-    res.send({ hi: 'there' });  //send this response
+const mongoose = require('mongoose');
 
-});
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+
+
+//mongoose.connect(keys.mongoURI);
+//const authRoutes = require('./routes/authRoutes');
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+//authRoutes(app);
+require('./routes/authRoutes')(app); // require auth routes returns a function with a call back functioh  with app function
+//inside one app you can have mny
+// Route Handler
+//app.get('/', (req, res) => {  // req incoming request res is the response
+//  res.send({ hi: 'there' });  //send this response
+
+
+//});
 const PORT = process.env.PORT || 5000; // process.env is there for Heroku and 5000 is there for the environment (default value)
 app.listen(PORT); //tells node that it listens to port 5000
---
+++ services/passport.js
@@ -0,0 +1,47 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        });
+});
+
+
+passport.use(new GoogleStrategy({
+
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+
+}, (accessToken, refreshshToken, profile, done) => {
+    User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+
+            done(null, existingUser);
+            // console.log(existingUser);
+        }
+        else {
+            new User({ googleId: profile.id }).save().then(user => done(null, user));
+            //  console.log(user);
+
+        }
+    });
+
+    // console.log('accessToken', accessToken);
+    // console.log('refreshshToken', refreshshToken);
+    // console.log('profile:', profile);
+    // console.log('done', done);
+
+
+}
+));  // passport.use is telling pasport that there is a new strategy available

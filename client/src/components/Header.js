8f83d9f5d04cf156c3dd36efea05ce63b29e0d67
Nalaka <ndvrocks@gmail.com>
2020-06-02 15:06:25 +0100

    payments

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,27 +2,25 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Payments from './Payments';
+
 class Header extends Component {
 
     renderContent() {
+
         switch (this.props.auth) {
             case null:
                 return;
             case false:
-                return <li><a href="/auth/google">Log in with Google</a></li>;
-
-
+                return <li><a href="/auth/google">Login With Google</a></li>;
             default:
                 return [
-                    <li><Payments /></li>,
-                    <li><a href="/api/logout">Log out</a></li>
 
-
+                    <li ><Payments /></li>,
+                    <li ><a href="/api/logout">Logout</a></li>
                 ];
-
         }
 
-    }
+    };
     render() {
 
         return (
